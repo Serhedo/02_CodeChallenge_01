1a3499615af82598dd7bd4027d407ba20081f19c
Serhedo <serhat.alpayci@gmx.de>
2023-05-09 15:42:55 +0200

    Challenge gelöst

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,24 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+
+
+
+
+
+let x, y, summe;
+
+x = prompt("Bitte gib die 1. Zahl ein");
+y = prompt("Bitte gib die 2. Zahl ein");
+
+// x = parseInt(x); //Umwandlung von string in ganze Zahl
+// y = parseInt(y); //Umwandlung von string in ganze Zahl
+
+x = parseFloat(x); //Umwandlung von string in float
+y = parseFloat(y); //Umwandlung von string in float
+
+summe = x + y;
+
+
+console.log("Die Summe der Zahlen ist " + summe);
+
